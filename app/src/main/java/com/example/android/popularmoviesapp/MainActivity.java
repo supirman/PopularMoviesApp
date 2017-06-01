8a0eb568ea731579183f86b7191c97b0d1fc7fe8
Firman Rosdiansyah <firman.droid@gmail.com>
2017-06-01 15:55:01 +0700

    initial

---
+++ app/src/main/java/com/example/android/popularmoviesapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.popularmoviesapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings,menu);
+        return true;
+    }
+}
